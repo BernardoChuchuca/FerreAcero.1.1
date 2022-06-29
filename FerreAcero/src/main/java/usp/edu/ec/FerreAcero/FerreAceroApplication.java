865f0b57b22c8d36a58cbe142c50435ff8d3560f
BernardoChuchuca <47090582+BernardoChuchuca@users.noreply.github.com>
2022-06-29 11:36:49 -0500

    update
    update

---
+++ FerreAcero/src/main/java/usp/edu/ec/FerreAcero/FerreAceroApplication.java
@@ -0,0 +1,13 @@
+package usp.edu.ec.FerreAcero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreAceroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreAceroApplication.class, args);
+	}
+
+}
