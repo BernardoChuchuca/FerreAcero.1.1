5fb122aab7eb5d0c0ac2573a460305ce85b24747
paulAAV98 <59033526+paulAAV98@users.noreply.github.com>
2022-06-29 20:37:21 -0500

    update
    update

--- FerreAcero/src/main/java/usp/edu/ec/FerreAcero/controladores/CarritoControlador.java
+++ FerreAcero/src/main/java/usp/edu/ec/FerreAcero/controladores/CarritoControlador.java
@@ -1,4 +1,36 @@
 package usp.edu.ec.FerreAcero.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import usp.edu.ec.FerreAcero.entidades.Carrito;
+import usp.edu.ec.FerreAcero.entidades.peticiones.carrito.CrearCarrito;
+import usp.edu.ec.FerreAcero.servicios.CarritoServicio;
+
+import java.util.List;
+
 public class CarritoControlador {
+
+    private CarritoServicio carritoServicio;
+
+    @Autowired
+    public void setCarritoServicio(CarritoServicio carritoServicio){
+
+        this.carritoServicio = carritoServicio;
+
+    }
+
+    @GetMapping("/carritos")
+    public ResponseEntity<List<Carrito>> getAllCarrito(){
+
+        List<Carrito> listaCarrito=carritoServicio.findAll();
+
+        return new ResponseEntity<List<Carrito>>(listaCarrito, HttpStatus.OK);
+    }
+
+
 }
--- FerreAcero/src/main/java/usp/edu/ec/FerreAcero/entidades/Carrito.java
+++ FerreAcero/src/main/java/usp/edu/ec/FerreAcero/entidades/Carrito.java
@@ -1,6 +1,97 @@
 package usp.edu.ec.FerreAcero.entidades;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
+@Entity
 public class Carrito implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private int numero;
+    private Date fecha;
+    @ManyToOne
+    @JoinColumn
+    private Persona persona;
+    @ManyToOne
+    @JoinColumn
+    private Producto producto;
+
+    public Carrito(int id, int numero, Date fecha, Persona persona, Producto producto) {
+        this.id = id;
+        this.numero = numero;
+        this.fecha = fecha;
+        this.persona = persona;
+        this.producto = producto;
+    }
+
+    public Carrito() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Persona getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Persona persona) {
+        this.persona = persona;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carrito carrito = (Carrito) o;
+        return id == carrito.id && numero == carrito.numero && Objects.equals(fecha, carrito.fecha) && Objects.equals(persona, carrito.persona) && Objects.equals(producto, carrito.producto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, numero, fecha, persona, producto);
+    }
+
+    @Override
+    public String toString() {
+        return "Carrito{" +
+                "id=" + id +
+                ", numero=" + numero +
+                ", fecha=" + fecha +
+                ", persona=" + persona +
+                ", producto=" + producto +
+                '}';
+    }
 }
+
