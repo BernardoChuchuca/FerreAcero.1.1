ef1b268243a5ed85772c95e8fe30c55c0e4db489
jonathanMorocho2 <73446057+jonathanMorocho2@users.noreply.github.com>
2022-06-30 01:41:52 -0500

    update
    update

--- FerreAcero/src/main/java/usp/edu/ec/FerreAcero/servicios/TarjetaServicio.java
+++ FerreAcero/src/main/java/usp/edu/ec/FerreAcero/servicios/TarjetaServicio.java
@@ -2,7 +2,9 @@ package usp.edu.ec.FerreAcero.servicios;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import usp.edu.ec.FerreAcero.entidades.Carrito;
 import usp.edu.ec.FerreAcero.entidades.Persona;
+import usp.edu.ec.FerreAcero.entidades.Producto;
 import usp.edu.ec.FerreAcero.entidades.TarjetaCredito;
 import usp.edu.ec.FerreAcero.repositorios.PersonaRespositorio;
 import usp.edu.ec.FerreAcero.repositorios.TarjetaRepositorio;
@@ -19,6 +21,41 @@ public class TarjetaServicio {
     public List<TarjetaCredito> findAll(){
 
         return (List<TarjetaCredito>)tarjetaRepositorio.findAll() ;
+    }
+
+    public TarjetaCredito consultaDatos(String numeroTarjeta){
+
+        return (TarjetaCredito) tarjetaRepositorio.findTarjetaCreditoByNumeroTarjeta(numeroTarjeta);
+    }
+
+    public TarjetaCredito consultaDatosTarjeta(String numeroTarjeta){
+
+        return (TarjetaCredito) tarjetaRepositorio.findTarjetaCreditoByNumeroTarjeta(numeroTarjeta);
+    }
+
+    public void save(TarjetaCredito TarjetaCredito){
+
+        tarjetaRepositorio.save(TarjetaCredito);
+    }
+
+
+
+    public TarjetaCredito findByNumeroTarjeta (String numeroTarjeta){
+
+        return (TarjetaCredito) tarjetaRepositorio.findTarjetaCreditoByNumeroTarjeta(numeroTarjeta);
+
+    }
+
+    public Optional<TarjetaCredito> findById(int id){
+
+        return  (Optional<TarjetaCredito>) tarjetaRepositorio.findById(id);
+
+    }
+
+
+    public void delete(int id) {
+        tarjetaRepositorio.deleteById(id);
+
     }
 
 
